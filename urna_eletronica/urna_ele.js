327fc5b74685d6dd973338141f05afd28a54b0c7
David de Deus Mesquita <72158476+DDEUSM@users.noreply.github.com>
2022-12-23 12:27:13 -0300

    Fix Bug da urna eletrônica

--- urna_eletronica/urna_ele.js
+++ urna_eletronica/urna_ele.js
@@ -1,36 +1,37 @@
 
-let slc = (element)=> {return document.querySelector(element)};
-let slcAll = (element)=>{return document.querySelectorAll(element)};
+let slc = (element) => { return document.querySelector(element) };
+let slcAll = (element) => { return document.querySelectorAll(element) };
 let div_digitos = '';
 let digitos = '';
-let voto;
+let voto = null;
 let votos = [];
 let etapa = 0;
 let block = false;
 
 slcAll('.teclado button').forEach((element) => {
-    element.addEventListener('click', ()=> {
-        if(digitos.length < candidatos[etapa].digitos){
+    element.addEventListener('click', () => {
+        if (digitos.length < candidatos[etapa].digitos) {
             digitos += element.getAttribute('data-key');
             let digito = element.getAttribute('data-key');
             slc('.info-de-tecla').style.display = 'block';
-            
+
             atualizaTela(digito);
         };
     });
 });
 
-function preparaTela(){
+function preparaTela() {
     div_digitos = '';
-    for(let i=0; i<candidatos[etapa].digitos; i++){
-        if(i==0){
+    for (let i = 0; i < candidatos[etapa].digitos; i++) {
+        if (i == 0) {
             div_digitos += '<div class="pisca"></div>';
-        }else {
+        } else {
             div_digitos += '<div></div>';
         }
     }
     slc('.voto-branco').style.display = 'none';
     slc('.voto-nulo').style.display = 'none';
+    slc('.info-de-tecla').style.display = 'none';
     slc('.painel-info').style.display = 'flex';
     slc('.title').innerHTML = 'SEU VOTO PARA';
     slc('.cargo-eletivo').innerHTML = candidatos[etapa].título;
@@ -39,7 +40,7 @@ function preparaTela(){
     slc('.candidato img').src = '';
     slc('.vice-candidato img').src = '';
     slc('.nome-vice').innerHTML = '';
-    slcAll('.cargo-politico').forEach((element)=> element.innerHTML = '');
+    slcAll('.cargo-politico').forEach((element) => element.innerHTML = '');
     slc('.numeros-digitados').innerHTML = div_digitos;
 };
 
@@ -47,17 +48,20 @@ function atualizaTela(dg) {
     let div_digit = slcAll('.numeros-digitados div')[digitos.length - 1]
     div_digit.classList.remove('pisca');
     div_digit.innerHTML = dg;
-    if(div_digit.nextElementSibling){
+    if (div_digit.nextElementSibling) {
         div_digit.nextElementSibling.classList.add('pisca');
     }
-    candidatos[etapa].todos.map((politico)=>{
-        if(politico.numero == digitos){
+    candidatos[etapa].todos.map((politico) => {
+
+        if (politico.numero == digitos) {
+
             voto = politico;
+
             slc('.nome-candidato').innerHTML = `NOME: ${politico.nome}`;
             slc('.partido-candidato').innerHTML = `PARTIDO: ${politico.partido}`;
             slc('.candidato img').src = politico.img;
             let cargos = slcAll('.cargo-politico');
-            if("vice" in politico) {
+            if ("vice" in politico) {
                 cargos[0].innerHTML = candidatos[etapa].título;
                 cargos[1].innerHTML = 'Vice-Presidente';
                 slc('.vice-candidato img').src = politico.vice.img;
@@ -65,49 +69,53 @@ function atualizaTela(dg) {
             } else {
                 cargos[0].innerHTML = candidatos[etapa].título;
             };
-            
+
         };
-        if(digitos.length == candidatos[etapa].digitos){
-            if(voto == null) {
-                voto = 'voto nulo';
-                slc('.voto-nulo').style.display = 'flex';
-            };
-        }
+
     });
+    if (digitos.length == candidatos[etapa].digitos) {
+        if (voto == null) {
+
+            voto = 'voto nulo';
+            slc('.voto-nulo').style.display = 'flex';
+        };
+    }
 };
 
-slcAll('.buttons-de-finalizar div').forEach((element)=> {
-    element.addEventListener('click', ()=> {
-        if(votos.length < candidatos.length){
-            if(element.getAttribute('class') == 'btn corrige'){
+slcAll('.buttons-de-finalizar div').forEach((element) => {
+    element.addEventListener('click', () => {
+        if (votos.length < candidatos.length) {
+            if (element.getAttribute('class') == 'btn corrige') {
                 digitos = '';
                 voto = null;
                 preparaTela()
             }
 
-            if(element.getAttribute('class') == 'btn branco'){
+            if (element.getAttribute('class') == 'btn branco') {
                 voto = 'Voto em Branco';
                 slc('.painel-info').style.display = 'none';
-                
-                slc('.voto-branco').style.display = 'flex';                
+
+                slc('.voto-branco').style.display = 'flex';
+                slc('.info-de-tecla').style.display = 'block';
             }
-            if(digitos.length == candidatos[etapa].digitos || voto == 'Voto em Branco') {                
-                if(element.getAttribute('class') == 'btn confirma'){
+            if (digitos.length == candidatos[etapa].digitos || voto == 'Voto em Branco') {
+                if (element.getAttribute('class') == 'btn confirma') {
                     votos.push(voto);
                     voto = null;
                     digitos = '';
-                    if(votos.length == 2){
+                    if (votos.length == 2) {
                         block = true;
-                    }                    
-                    if(block == false) {
-                        etapa ++;
-                        preparaTela();                        
-                    }else {
+                    }
+                    if (block == false) {
+                        etapa++;
+                        preparaTela();
+                    } else {
+                        slc('.voto-branco').style.display = 'none';
                         slc('.painel-info').style.display = 'none';
                         slc('.info-de-tecla').style.display = 'none';
                         slc('.fim').classList.add('pisca');
                         slc('.fim').style.display = 'flex';
-                        
+                        console.log(votos);
                     }
                 }
             }
